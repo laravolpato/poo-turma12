b09bf2d2c0bc4399b819fd2fdd686419180faf26
Alexandre Agustini <alexandre.agustini@pucrs.br>
2025-08-11 15:51:12 +0000

    Criação do projeto Vendas

---
+++ VendasPrj/src/main/java/poo/aagustini/App.java
@@ -0,0 +1,7 @@
+package poo.aagustini;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto VendasPrj criado com sucesso.");
+    }
+}
